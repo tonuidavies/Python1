eda53d9073cf21dee24b7fe6f86e2a4747cbf03f
tonuidavies <tonuidavies@gmail.com>
2016-05-04 11:03:45 +0300

    calculating delta time of 10**9 seconds from now

--- one.py
+++ one.py
@@ -1,8 +1,8 @@
-
+#! user/bin/python
 def fib2(n): # return Fibonacci series up to n
     result = []
     a, b = 0, 1
-    while b < n:
+    while b < 10:
         result.append(b)
         a, b = b, a+b
-    return result
+return result
